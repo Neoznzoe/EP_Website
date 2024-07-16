51529eab9af314823b95de912c7dad5ffec3a565
Victor <bessonvictor59@gmail.com>
2024-07-16 15:12:22 +0200

    feat: results page

--- src/pages/palmares/palmares.tsx
+++ src/pages/palmares/palmares.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import SectionHeader from '../../component/sectionHeader/sectionHeader';
-import './palmares.css'
+import './palmares.css';
 import Footer from '../../component/footer/footer';
 
 const PalmaresPage: React.FC = () => {
   return (
     <div>
-      <SectionHeader></SectionHeader>
+      <SectionHeader />
       <div className="content">
         <div className="header">
           <h1>Résultats</h1>
@@ -19,14 +19,38 @@ const PalmaresPage: React.FC = () => {
             <hr />
             <h2>European cup</h2>
             <div className="euroCupContainer">
-              
+              <div className="victory">
+                <h1>Victoire</h1>
+                <span>1</span>
+              </div>
+              <div className="podium">
+                <h1>Podium</h1>
+                <span>3</span>
+              </div>
+              <div className="top10">
+                <h1>Top 10</h1>
+                <span>10</span>
+              </div>
             </div>
           </div>
           <div className="worldCupTitle">
             <h1>Coupe du monde</h1>
             <hr />
             <h2>World cup</h2>
-            <div className="worldCupContainer"></div>
+            <div className="worldCupContainer">
+              <div className="top15">
+                <h1>Top 15</h1>
+                <span>1</span>
+              </div>
+              <div className="top30">
+                <h1>Top 30</h1>
+                <span>3</span>
+              </div>
+              <div className="top50">
+                <h1>Top 50</h1>
+                <span>13</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
